e540a044e22a3946e9031cf8e80324dc0b1dfae5
Juan Jose <alastorfender29@gmail.com>
2025-03-03 16:34:32 +0100

    strings added

---
+++ basic_mio/06-strings.js
@@ -0,0 +1,21 @@
+//Concatenación
+
+let myName = "Juan José"
+let greeting = "Hola, " + myName + "!"
+console.log(greeting)
+console.log(typeof greeting)
+
+//Longitud
+console.log(greeting.length)
+
+//Acceso a caracteres
+console.log(greeting[0])
+console.log(greeting[2])
+console.log(greeting[15])
+
+//Metodos comunes
+console.log(greeting.toUpperCase())
+console.log(greeting.toLowerCase())
+console.log(greeting.indexOf("Hola"))
+console.log(greeting.length)
+console.log(greeting.length)
