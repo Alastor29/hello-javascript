1fa1254723a593ad32fd46d9197822acb1881fa4
Juan Jose <alastorfender29@gmail.com>
2025-03-04 20:51:05 +0100

    subida

--- basic_mio/06-strings.js
+++ basic_mio/06-strings.js
@@ -17,5 +17,24 @@ console.log(greeting[15])
 console.log(greeting.toUpperCase())
 console.log(greeting.toLowerCase())
 console.log(greeting.indexOf("Hola"))
-console.log(greeting.length)
-console.log(greeting.length)
+console.log(greeting.indexOf("Juan José"))
+console.log(greeting.indexOf("!"))
+console.log(greeting.includes("Hola"))
+console.log(greeting.includes("Juan José"))
+console.log(greeting.includes("!"))
+console.log(greeting.includes("Pepe"))
+console.log(greeting.slice(0,10))//excluye el final o sea serian 9
+console.log(greeting.replace("Juan José","MoureDev"))
+
+//Template literals (plantillas literales)
+let message = `Hola, este es mi 
+curso de Javascript`
+
+console.log(message)
+
+let mail = "alastorfender29@gmail.com"
+
+console.log(`Hola, ${myName}! Tu email es ${mail}`)
+
+
+
